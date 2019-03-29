d8ac4e16f4ba8ec1dc468f7a9645324f53a46663
Phil Goo Kang <philgookang@gmail.com>
2019-03-29 12:50:13 +0900

    Changed testing to be more diverse

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ from hw1 import *
 
 if __name__=="__main__":
 
-    a = list(set([random.randint(1, 1000000) for i in range(1000000)]))
-    n = len(a)
-    for i in range(3000):
+    for i in range(10):
+
+        a = list(set([random.randint(1, 1000000) for i in range(1000000)]))
+        n = len(a)
+
         k = random.randint(1, n - 1)
         random.shuffle(a)
 
         ans1 = randomized_select(a, n, k)
         if checker(a, n, k, ans1) == True:
-            print("correct")
+            print(i, "correct")
         else:
-            print('incorrect')
+            print(i, 'incorrect')
         ans2 = deterministic_select(a, n, k)
         if checker(a, n, k, ans2) == True:
-            print("correct")
+            print(i,"correct")
         else:
-            print('incorrect')
-        break
+            print(i, 'incorrect')
 
     print("All Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
