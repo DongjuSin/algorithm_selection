d58b5313b689f3540f330398b9c7502d19160fdf
Phil Goo Kang <philgookang@gmail.com>
2019-03-29 09:15:18 +0900

    Created large testable system

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from hw1 import *
 
 if __name__=="__main__":
+
+    a = list(set([random.randint(1, 1000000) for i in range(1000000)]))
+    n = len(a)
+    for i in range(3000):
+        k = random.randint(1, n - 1)
+        random.shuffle(a)
+
+        ans1 = randomized_select(a, n, k)
+        if checker(a, n, k, ans1) == True:
+            print("correct")
+        else:
+            print('incorrect')
+        ans2 = deterministic_select(a, n, k)
+        if checker(a, n, k, ans2) == True:
+            print("correct")
+        else:
+            print('incorrect')
+        break
+
+    print("All Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    '''
     a = [ 4, 5, 1, 3, 6, 7, 9, 10, 2, 8 ]
     n = 10
     k = 4
-    ans1 = randomized_select(a, n, k)
-    print(ans1)
-
-    a = [4, 5, 1, 3, 6, 14, 22, 7, 9, 10, 2, 8]
-    ans2 = deterministic_select(a, n, k)
-    print(ans2)
-
-    '''
     ans1 = randomized_select(a, n, k)
     if checker(a, n, k, ans1)==True:
         print('correct')
