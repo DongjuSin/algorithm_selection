56fb35f85e61d6b2f3031de0165e4e9df890be0b
Phil Goo Kang <philgookang@gmail.com>
2019-03-31 21:51:19 +0900

    Added new testing methods

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from hw1 import *
+import pickle
 
 if __name__=="__main__":
 
+    # with open("ff.bin", 'rb') as pickle_file:
+    #     a = pickle.load(pickle_file)
+    #
+    #     if len(a):
+    #         pass
+
     for i in range(3000):
 
-        # a = list(set([random.randint(1, 1000000000) for i in range(2000000)]))
-        a = list(set([random.randint(1, 200) for i in range(100)]))
+        a = list(set([random.randint(1, 1000000000) for i in range(2000000)]))
+        # a = list(set([random.randint(1, 200) for i in range(100)]))
         n = len(a)
 
         k = random.randint(1, n - 1)
@@ -13,14 +20,22 @@ if __name__=="__main__":
 
         ans1 = randomized_select(a, n, k)
         if checker(a, n, k, ans1) == True:
-            print(i, "correct", k)
+            # print(i, "correct", k)
+            pass
         else:
+            with open("ff.bin", 'wb') as pickle_file:
+                pickle.dump({"k": k, "data": n}, pickle_file)
             print(i, 'incorrect =================================================')
+            break
         ans2 = deterministic_select(a, n, k)
         if checker(a, n, k, ans2) == True:
-            print(i,"correct", k)
+            # print(i,"correct", k)
+            pass
         else:
+            with open("ff.bin", 'wb') as pickle_file:
+                pickle.dump({"k": k, "data": n}, pickle_file)
             print(i, 'incorrect =================================================')
+            break
 
     print("All Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
