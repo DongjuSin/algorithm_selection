db46e564de217db1b63364f7d6999639b30ca8a0
Phil Goo Kang <philgookang@gmail.com>
2019-04-03 11:17:25 +0900

    Added code to create custom dataset

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import datetime
 
 if __name__=="__main__":
 
+    # a = list(set([i for i in range(1, 10001)]))
+    # random.shuffle(a)
+    #
+    # with open("custom_2.txt", "w") as file:
+    #     file.write("10000 0\n")
+    #     for i in a:
+    #         file.write(str(i) + "\n")
+
     # with open("ff.bin", 'rb') as pickle_file:
     #     a = pickle.load(pickle_file)
     #
@@ -15,14 +23,16 @@ if __name__=="__main__":
         lst_length = random.randint(10, 2000000)
         a = list(set([random.randint(1, 1000000000) for i in range(lst_length)]))
         # a = list(set([random.randint(1, 200) for i in range(100)]))
+        # a = [1,2,3,4,5,6,7,8,9]
         n = len(a)
 
         k = random.randint(0, n - 1)
         random.shuffle(a)
+        k = 8
 
         ans1 = randomized_select(a, n, k)
         if checker(a, n, k, ans1) == True:
-            # print(i, "correct", k)
+            print(i, "correct", k, ans1)
             now = datetime.datetime.now()
             print(str(now))
             pass
@@ -33,7 +43,7 @@ if __name__=="__main__":
             break
         ans2 = deterministic_select(a, n, k)
         if checker(a, n, k, ans2) == True:
-            # print(i,"correct", k)
+            print(i,"correct", k, ans2)
             now = datetime.datetime.now()
             print(str(now))
             pass
